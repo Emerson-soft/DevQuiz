2fa48878a97dab8c2581544214f4ee3c00355a56
Emerson <emerson_10_s@hotmail.com>
2021-10-19 23:21:55 -0300

    fix

--- lib/home/widgets/chart/chart_widget.dart
+++ lib/home/widgets/chart/chart_widget.dart
@@ -1,9 +1,15 @@
-import 'package:DevQuiz/core/app_colors.dart';
-import 'package:DevQuiz/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
-class  ChartWidget extends StatelessWidget {
-  const ChartWidget({ Key? key }) : super(key: key);
+import 'package:DevQuiz/core/app_colors.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/models/user_model.dart';
+
+class ChartWidget extends StatelessWidget {
+  final double score;
+  const ChartWidget({
+    Key? key,
+    required this.score,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +23,13 @@ class  ChartWidget extends StatelessWidget {
             width: 80,
             child: CircularProgressIndicator(
               strokeWidth: 10,
-              value: .75,
+              value: score,
               backgroundColor: AppColors.chartSecondary,
               valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
             ),
           ),
         ),
-        Center(child: Text("75%", style: AppTextStyles.heading,))
+        Center(child: Text('${score * 100}%', style: AppTextStyles.heading,))
       ],),
     );
   }
--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -1,10 +1,16 @@
-import 'package:DevQuiz/core/app_colors.dart';
-import 'package:DevQuiz/core/app_text_styles.dart';
-import 'package:DevQuiz/home/widgets/chart/chart_widget.dart';
 import 'package:flutter/material.dart';
 
+import 'package:DevQuiz/core/app_colors.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/models/user_model.dart';
+import 'package:DevQuiz/home/widgets/chart/chart_widget.dart';
+
 class ScoreCardWidget extends StatelessWidget {
-  const ScoreCardWidget({ Key? key }) : super(key: key);
+  final UserModel user;
+  const ScoreCardWidget({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +30,9 @@ class ScoreCardWidget extends StatelessWidget {
             children: [
               Expanded(
                 flex: 1,
-                child: ChartWidget(),
+                child: ChartWidget(
+                  score: user.score,
+                ),
               ),
               Expanded(
                 flex: 3,
@@ -39,7 +47,7 @@ class ScoreCardWidget extends StatelessWidget {
                       style: AppTextStyles.heading
                     ),
                     Text(
-                      'Complete os desafios e acanve em conhecimento',
+                      'Complete os desafios e avançe em conhecimento',
                       style: AppTextStyles.body
                     )
                   ],),
